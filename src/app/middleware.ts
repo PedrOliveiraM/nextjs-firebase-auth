01e5e9bd432e20049c7b8f531616c97c10bfab0c
PedrOliveiraM <pedropucmont@gmail.com>
2025-02-08 00:11:10 -0300

    feat: add custom 404 page with Rive animation and middleware for authentication

---
+++ src/app/middleware.ts
@@ -0,0 +1,3 @@
+// eslint-disable-next-line prettier/prettier
+export { auth as middleware } from '@/auth/auth';
+
---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { useRive } from '@rive-app/react-canvas'
+
+export default function NotFound() {
+  const STATE_MACHINE_NAME = 'SM_ComingSoon'
+
+  const { RiveComponent } = useRive({
+    src: '/river/404_purple.riv',
+    stateMachines: STATE_MACHINE_NAME,
+    autoplay: true,
+  })
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <RiveComponent style={{ width: '500px', height: '500px' }} />
+      <h1 className="text-center text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="text-center text-lg">
+        The page you are looking for might have been removed, had its name
+        changed or is temporarily unavailable.
+      </p>
+      <Button>
+        <a href="/" className="text-white">
+          Go to Home
+        </a>
+      </Button>
+    </div>
+  )
+}
