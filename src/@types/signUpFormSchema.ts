fe976d1187bdaee0857aaebf15a95233a5d0989d
PedrOliveiraM <pedropucmont@gmail.com>
2025-02-12 08:40:31 -0300

    feat: add sign-up form schema with validation rules for username, email, and password

--- src/@types/signUpFormSchema.ts
+++ src/@types/signUpFormSchema.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod'
 
-export const signInFormSchema = z.object({
+export const signUpFormSchema = z.object({
+  username: z
+    .string()
+    .nonempty('This field is required')
+    .trim()
+    .min(3, 'Username must be at least 3 characters long'),
   email: z
     .string({ message: 'Is not a string' })
     .email({ message: 'This email is not valid' })
@@ -12,4 +17,4 @@ export const signInFormSchema = z.object({
     .nonempty('This field is required'),
 })
 
-export type signInFormData = z.infer<typeof signInFormSchema>
+export type signUpFormData = z.infer<typeof signUpFormSchema>
