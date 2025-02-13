2138d199cb1ecb7d0df4fa8a3e409e35f7cf9cb1
PedrOliveiraM <pedropucmont@gmail.com>
2025-02-13 15:38:11 -0300

    feat: add credentials provider for email and password authentication with Firebase

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,56 @@
+import { signInFormSchema } from '@/@types/signInFormSchema'
 import { FirestoreAdapter } from '@auth/firebase-adapter'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import NextAuth from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
 import GitHub from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
+import { ZodError } from 'zod'
 import { firebaseCert } from './firebase'
+import clientApp from './firebaseClient'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, GitHub],
+  providers: [
+    Google,
+    GitHub,
+    Credentials({
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials) => {
+        try {
+          if (!credentials?.email || !credentials?.password) {
+            console.error('Missing email or password')
+            return null
+          }
+
+          const { email, password } =
+            await signInFormSchema.parseAsync(credentials)
+
+          const auth = getAuth(clientApp)
+          const userCredential = await signInWithEmailAndPassword(
+            auth,
+            email,
+            password,
+          )
+
+          const user = userCredential.user
+          if (user) {
+            return { id: user.uid, email: user.email }
+          }
+
+          return null
+        } catch (error) {
+          if (error instanceof ZodError) {
+            console.error('Zod Validation Error:', error.errors)
+            return null
+          }
+          return null
+        }
+      },
+    }),
+  ],
   adapter: FirestoreAdapter({
     credential: firebaseCert,
   }),
